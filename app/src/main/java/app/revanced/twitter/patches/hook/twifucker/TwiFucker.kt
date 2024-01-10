a94ba619afe99121d85149380adbe44bcc82df4e
yuuki14202028 <yuuki14202028@gmail.com>
2024-01-10 12:18:45 +0900

    feat: logging

--- app/src/main/java/app/revanced/twitter/patches/hook/twifucker/TwiFucker.kt
+++ app/src/main/java/app/revanced/twitter/patches/hook/twifucker/TwiFucker.kt
@@ -209,6 +209,17 @@ internal object TwiFucker {
         ?.optJSONObject("core")
         ?.optJSONObject("user_result")
         ?.optJSONObject("result")
+        ?.optBoolean("is_blue_verified") ?: optJSONObject("content")
+        ?.optJSONObject("content")
+        ?.optJSONObject("tweetResult")
+        ?.optJSONObject("result")
+        ?.optJSONObject("legacy")
+        ?.optJSONObject("retweeted_status_result")
+        ?.optJSONObject("result")
+        ?.optJSONObject("tweet")
+        ?.optJSONObject("core")
+        ?.optJSONObject("user_result")
+        ?.optJSONObject("result")
         ?.optBoolean("is_blue_verified") ?: false
 
     fun JSONArray.entriesRemoveWhoToBlue() {
@@ -219,6 +230,16 @@ internal object TwiFucker {
                     ?.optJSONObject("content")
                     ?.optJSONObject("tweetResult")
                     ?.optJSONObject("result")
+            }")
+            Log.d("ReVanced", "にゃん~~ RT?:${entry.entryIsWhoToBlue()}: ${
+                entry.optJSONObject("content")
+                    ?.optJSONObject("content")
+                    ?.optJSONObject("tweetResult")
+                    ?.optJSONObject("result")
+                    ?.optJSONObject("legacy")
+                    ?.optJSONObject("retweeted_status_result")
+                    ?.optJSONObject("result")
+                    ?.optJSONObject("tweet")
             }")
             if (!entry.entryIsWhoToBlue()) return@forEachIndexed
 
